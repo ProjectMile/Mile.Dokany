c5e77cd3dded2e64e3486c6ee553e7bb9a7c70ea
MouriNaruto <Kenji.Mouri@outlook.com>
2024-10-11 23:52:44 +0800

    Add Mile.Dokany project.

---
+++ Mile.Dokany/Mile.Dokany.cpp
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   Mile.Dokany
+ * FILE:      Mile.Dokany.cpp
+ * PURPOSE:   Implementation for Mile.Dokany
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <Windows.h>
+
+EXTERN_C int WINAPI MileSamplesTestFunction()
+{
+    return 0;
+}
